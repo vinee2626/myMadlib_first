b4565d3b28c7ffd1da18c0dbed5ac4128c844833
vinee2626 <76570780+vinee2626@users.noreply.github.com>
2021-05-08 18:13:42 +0530

    Add files via upload

---
+++ madlib4.py
@@ -0,0 +1,26 @@
+adj1 = input()
+adj2 = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+verb4 = input("Verb: ")
+name = input("Name: ")
+noun1 = input("Noun: ")
+liquid = input("Liquid: ")
+verb5 = input("Verb: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+noun4 = input("Noun: ")
+
+madlib = f"""It was a {adj1}, cold November day. I\n
+woke up to the {adj2} smell of {verb1}\n
+roasting in the {verb2} downstairs. I \n
+{verb3} down the stairs to see if I could \n
+help {verb4} the dinner. My mom said,\n
+see if {name} needs a fresh {noun1}. So I \n
+carried a tray of glasses full of {liquid} into \n
+the {verb5} room. When I got there, I\n
+couldn't believe my {noun2}! There were\n
+{noun3} on the {noun4}!"""
+
+print(madlib)
